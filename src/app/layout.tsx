d7276b83e51039b3a764fbf4da1c4b15e628aabe
Komodo-Debian <hakkerby01@gmail.com>
2025-06-09 09:33:08 +0700

    created navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 // import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/custom/navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
         {/* <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }> */}
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
         {/* </ClerkProvider> */}
       </body>
